1bb3d96456a555d494f886f1fa22c8cfd267dc4e
GaryGaster <gastergary@gmail.com>
2021-01-12 01:23:39 +0100

    Add profile account creation

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView, password_success, ShowProfilePageView
-
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, password_success, ShowProfilePageView, \
+    EditProfilePageView, CreateProfilePageView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
@@ -8,5 +8,7 @@ urlpatterns = [
     path('password/', PasswordsChangeView.as_view(), name='change-password'),
     path('password-success/', password_success, name='password-success'),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show-profile-page'),
+    path('<int:pk>/edit-profile-page/', EditProfilePageView.as_view(), name='edit-profile-page'),
+    path('create-profile-page/', CreateProfilePageView.as_view(), name='create-profile-page'),
 
 ]
--- main/models.py
+++ main/models.py
@@ -18,6 +18,9 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.user)
 
+    def get_absolute_url(self):
+        return reverse('blog')
+
 
 
 class Post(models.Model):
