2380c93ac4e35ea2fd1a26dea00cb36f9a1a18fb
GaryGaster <gastergary@gmail.com>
2021-01-24 15:04:35 +0100

    Add password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from .views import UserRegisterView, UserEditView, PasswordsChangeView, password_success, ShowProfilePageView, \
-    EditProfilePageView, CreateProfilePageView
+    EditProfilePageView, CreateProfilePageView, PasswordsResetView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
@@ -10,5 +12,21 @@ urlpatterns = [
     path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show-profile-page'),
     path('<int:pk>/edit-profile-page/', EditProfilePageView.as_view(), name='edit-profile-page'),
     path('create-profile-page/', CreateProfilePageView.as_view(), name='create-profile-page'),
+    path('password-reset/',
+         PasswordsResetView.as_view(
+             template_name='accounts/password_reset.html'), name='password-reset'),
+    path('password-reset-sent/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='accounts/password_reset_sent.html'), name='password-reset-sent'),
+
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='accounts/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,35 @@
-from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.views import PasswordChangeView, PasswordResetView
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views import generic
+from django.views.decorators.csrf import csrf_protect
 from django.views.generic import DetailView, CreateView
 from main.models import Profile
 
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm, ProfilePageForm, EditProfilePageForm
 
+class PasswordsResetView(PasswordResetView):
+    from_email = 'pa.ko.doradca@gmail.com'
+    success_url = reverse_lazy('password-reset-sent')
+
+    @method_decorator(csrf_protect)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        opts = {
+            'use_https': self.request.is_secure(),
+            'token_generator': self.token_generator,
+            'from_email': self.from_email,
+            'email_template_name': self.email_template_name,
+            'subject_template_name': self.subject_template_name,
+            'request': self.request,
+            'html_email_template_name': self.html_email_template_name,
+            'extra_email_context': self.extra_email_context,
+        }
+        form.save(**opts)
+        return super().form_valid(form)
 
 class CreateProfilePageView(CreateView):
     model = Profile
